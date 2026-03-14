03e145f282760e41c2422f07ceba7279113d988f
markreyno <116620519+markreyno@users.noreply.github.com>
2026-03-14 10:55:24 -0700

    made emotion model and uploaded datasets

---
+++ models/test_models/own_emotion_anaylsis.py
@@ -0,0 +1,5 @@
+#This is a model that will analyze the emotion of the text.
+#We will be using GoEmotion dataset: https://research.google/blog/goemotions-a-dataset-for-fine-grained-emotion-classification/
+#https://github.com/google-research/google-research/tree/master/goemotions
+
+
